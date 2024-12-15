cc50d92bc545c3b6145082403f52adc2210ba2aa
deliciousNesquik <byte.in4matic@gmail.com>
2024-12-15 17:03:34 +0500

    Добавлены контролы которые используются в главном окне

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,25 +1,46 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ReactiveUI;
 using RFD.Models;
 
 namespace RFD.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ObservableObject
     {
-        public ObservableCollection<InfoBlock> InfoBlockList { get; }
+        public ObservableCollection<InfoBlock> InfoBlockList { get; private set; }
+        public ObservableCollection<InfoStatus> InfoStatusList { get; private set; }
+        
+        public double MagneticDeclination { get; private set; } 
+        public double ToolfaceOffset { get; private set; } 
 
         public MainWindowViewModel() 
         {
-            this.InfoBlockList =
+            InfoBlockList =
             [
                 new ("Высота блока", "-", "м"),
                 new ("Глубина долота", "-", "м"),
                 new ("Текущий забой", "-", "м"),
-                new ("Rop средний", "-", "м/ч"),
+                new ("TVD", "-", "м"),
                 new ("Расстояние до забоя", "-", "м"),
+                new ("Rop средний", "-", "м/ч"),
                 new ("Зенит", "-", "°"),
                 new ("Азимут", "-", "°"),
-                new ("TVD", "-", "м"),
             ];
+
+            InfoStatusList =
+            [
+                new ("Клинья", ""),
+                new ("Насос", ""),
+                new ("Забой", ""),
+            ];
+
+            MagneticDeclination = 0.00;
+            ToolfaceOffset = 0.00;
         }
     }
 }
