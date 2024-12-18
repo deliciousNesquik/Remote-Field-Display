a0f63a273e17cebb62235df5d5513e21b778760c
deliciousNesquik <byte.in4matic@gmail.com>
2024-12-18 21:52:58 +0500

    чет какие-то файлы не понятные

---
+++ Models/InfoStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace RFD.Models
+{
+    public class InfoStatus
+    {
+        public string Header { get; set; }
+        public string Status { get; set; }
+
+        public InfoStatus(string _Header, string _Status)
+        {
+            Header = _Header;
+            Status = _Status;
+        }
+    }
+}
