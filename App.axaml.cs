74636b0ee5df4c8f6448c89d35a6a0031ba1165d
deliciousNesquik <byte.in4matic@gmail.com>
2024-12-20 22:08:19 +0500

    добавлена библиотека для общения между Genesis и RFD

--- App.axaml.cs
+++ App.axaml.cs
@@ -3,11 +3,30 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using RFD.ViewModels;
 using RFD.Views;
+using NPFGEO.LWD.Net;
 
 namespace RFD
 {
     public partial class App : Application
     {
+        ServerListener _listener;
+        Client _client;
+        bool _needAutoReconnect = true;
+        public bool Connected { get => return _client.Connected; }
+        public string CurrentIpAddress
+        {
+            get
+            {
+                if (_client.Address == null)
+                    return string.Empty;
+                else
+                    return _client.Address.ToString();
+            }
+        }
+        CancellationTokenSource _cancelTokenSource;
+        CancellationToken _token;
+        
+        
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -26,7 +45,7 @@ namespace RFD
                 
                 //TODO
                 //Сделать полное подключение к серверу
-
+                mainWindowViewModel.IpAddress = CurrentIpAddress;
 
 
                 desktop.MainWindow = mainWindow;
